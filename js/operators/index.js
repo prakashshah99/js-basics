b32a47333ad46c2d40ddbb269d92db878cca8d95
prakashshah99 <shahprakash9853@gmail.com>
2024-04-13 21:10:08 +0545

    operators

--- js/operators/index.js
+++ js/operators/index.js
@@ -2,7 +2,12 @@ console.log("hello world");
 
 //variables
 
-const person = "prakash";
-const age = prompt("how old are you?");
+// const person = "prakash";
+// const age = prompt("how old are you?");
 
-console.log(person, age);
+// console.log(person, age);
+
+//write a program that store the user infrom about their birth year and birth calculator age    
+const alert ("uer inform about birth year and birth calculator age");
+const user = prompt("how old are you?");
+console.log(user);
