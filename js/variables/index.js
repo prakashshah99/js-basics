1ac62122fcc03a6871a7082107e8a8a99f38b94a
prakashshah99 <shahprakash9853@gmail.com>
2024-04-13 18:43:45 +0545

    html ,css and js setup variables

--- js/variables/index.js
+++ js/variables/index.js
@@ -1,1 +1,39 @@
 console.log("hello world");
+
+/*
+variables are defined in the following file and passed through to the constructor function below to ensure that variables are initialized properly when creating a new instance of the constructor function that will be called when the constructor function is called with the same arguments as the constructor function passed to constructor function above and the variables are initialized properly when creating a new instance of the constructor function that will   be called when the constructor function is called with the same arguments as the constructor function passed to constructor function above and the variables are initialized  properly when creating a new instance of the constructor function that will   be called when the constructor function is called with the same arguments as the constructor function passed to constructor function above and the variables are initialized properly when creating a new instance of the constructor function that will be called when     the constructor function is called with the same arguments as the constructor function passed           to constructor function above and the variables are initialized properly when creating a new instance of    the constructor function that will be called when the constructor function is called with the same                          arguments as the constructor function passed to constructor function above and the variables are initialized    variables as
+ 
+variables- named storages
+=) standard  ECMAScript 
+
+. es5
+-var
+.es6
+-let
+-const
+-var
+
+*/
+var person = "prakash"; //container = data
+var fullName = "prakash shah";
+var isAdult = "yes";
+
+let persons = "ram";
+const personName = "raven";
+console.log(person);
+
+/*interaction
+1.alert
+2. prompt
+3.confirm
+*/
+// alert = notefication
+alert("Good morning prakash ");
+
+//prompt = questionnaire
+const age = prompt("how old are you?");
+console.log(age);
+
+//confirm = yes/no question
+const isValid = confirm("are you above 18 years old?");
+console.log(isValid);
